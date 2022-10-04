b456eb169c892c0079d044293f78fb95d0678255
Dan Dobrin <dan.dobrin@gmail.com>
2022-10-04 08:38:56 -0400

    first commit native image - wip

---
+++ native/quotes-native/src/main/java/com/example/QuotesApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuotesApplication.class, args);
+  }
+}
