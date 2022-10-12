a117d31bb8c3b9090ca858ebb14d5c956cf933d1
asaikali <asaikali@gmail.com>
2022-10-12 02:23:32 -0400

    upgrade to spring cloud 2022.0.0-M5

--- discovery/eureka/eureka-message-service/src/main/java/com/example/messageservice/Quote.java
+++ discovery/eureka/eureka-message-service/src/main/java/com/example/messageservice/Quote.java
@@ -1,9 +1,9 @@
 package com.example.messageservice;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "quotes")
