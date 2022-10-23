fa079f996bf14247c2cb7a964d646fa9b81da60b
asaikali <asaikali@gmail.com>
2022-10-23 11:06:20 -0400

    update tracing project to micrometer tracing.

--- observability/tracing/billboard-client/src/main/java/com/example/BillboardController.java
+++ observability/tracing/billboard-client/src/main/java/com/example/BillboardController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import io.micrometer.tracing.SpanName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -15,15 +16,22 @@ public class BillboardController {
 
   public BillboardController(RestTemplateBuilder builder) {
 
-    // If the rest template builder is not used then the tracing won't work
+    // TLDR: Always use RestTemplateBuilder
+    //
+    // If ou create the rest template using new RestTemplate() instead of using
+    // a RestTemplateBuilder the created template will not be configured with
+    // the correct instrumentation setting you will not see any trace ids
+    // propagated to the backend message-service
     this.restTemplate = builder.build();
   }
 
   @GetMapping("/message")
+  @SpanName("get()")
   public String get() {
 
     logger.info("Calling message-service");
     Quote quote = restTemplate.getForObject("http://localhost:8081/", Quote.class);
+    logger.info("Got quote object",quote);
     return quote.getQuote() + " -- " + quote.getAuthor();
   }
 }
