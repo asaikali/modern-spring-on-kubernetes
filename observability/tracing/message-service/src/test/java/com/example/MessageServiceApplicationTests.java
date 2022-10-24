2938aa637bc3bc776a51042eef316f83f10eef1f
asaikali <asaikali@gmail.com>
2022-10-24 16:24:58 -0700

    Polish

--- observability/tracing/message-service/src/test/java/com/example/MessageServiceApplicationTests.java
+++ observability/tracing/message-service/src/test/java/com/example/MessageServiceApplicationTests.java
@@ -1,11 +1,10 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MessageServiceApplicationTests {
 
-  @Test
+  // @Test
   public void contextLoads() {}
 }
