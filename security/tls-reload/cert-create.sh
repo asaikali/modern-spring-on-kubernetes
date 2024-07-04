aa15af8c29c7ea3f9d8686fa7db101c320948524
Adib Saikali <asaikali@gmail.com>
2024-07-05 01:51:45 +0200

    Polish

--- security/tls-reload/cert-create.sh
+++ security/tls-reload/cert-create.sh
@@ -1,6 +1,14 @@
-openssl genpkey \
-  -algorithm rsa \
-  -out cert/demo.key
+
+# command to make an rsa based private key
+#openssl genpkey \
+#  -algorithm rsa \
+#  -out cert/demo.key
+
+# generate an elliptic curve private key
+openssl ecparam \
+  -genkey \
+  -name prime256v1 \
+  -out cert/demo.key \
 
 # Generate a self-signed X.509 certificate using the encrypted private key
 openssl req -x509 \
@@ -8,4 +16,5 @@ openssl req -x509 \
   -key cert/demo.key \
   -out cert/demo.crt \
   -days 1 \
-  -passin pass:changeit
+  -nodes
+
