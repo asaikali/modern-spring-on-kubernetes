bce008a78eb30f3b4e84805b345e726222119a36
Adib Saikali <asaikali@gmail.com>
2024-07-04 23:51:12 +0200

    Polish

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,2 +1,2 @@
-kapp deploy -a cert-manager -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.1/cert-manager.yaml -y
-kapp deploy -a envoy-gateway -f https://github.com/envoyproxy/gateway/releases/download/v1.0.2/install.yaml -y
+kapp deploy -a cert-manager -f static/cert-manager/cert-manager.yaml -y
+# kapp deploy -a envoy-gateway -f https://github.com/envoyproxy/gateway/releases/download/v1.0.2/install.yaml -y
