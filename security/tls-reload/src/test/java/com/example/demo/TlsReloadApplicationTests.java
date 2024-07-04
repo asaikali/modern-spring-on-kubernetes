03ec700ff52179369ee37507d1927be447754a59
Adib Saikali <asaikali@gmail.com>
2024-07-04 22:16:25 +0200

    Polish

--- security/tls-reload/src/test/java/com/example/demo/TlsReloadApplicationTests.java
+++ security/tls-reload/src/test/java/com/example/demo/TlsReloadApplicationTests.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TlsReloadApplicationTests {
 
-  @Test
+  // @Test
   void contextLoads() {}
 }
