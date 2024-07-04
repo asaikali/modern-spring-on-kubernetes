e915fcf42cc37ba47430074c4fc8de0182d31c21
Adib Saikali <asaikali@gmail.com>
2024-07-05 01:58:10 +0200

    Polish

--- security/tls-reload/cert-create.sh
+++ security/tls-reload/cert-create.sh
@@ -1,10 +1,19 @@
 
-# command to make an rsa based private key
+# command to make an rsa based private key (Works in Java 21)
+# Generated cert has Signature Algorithm: sha256WithRSAEncryption
 #openssl genpkey \
 #  -algorithm rsa \
 #  -out cert/demo.key
 
-# generate an elliptic curve private key
+# command to make an rsa based private key (Does not work in Java 21)
+# it claims there is no computable signature verification algorithm
+# Generate cert will have Signature Algorithm: ED25519
+#openssl genpkey \
+#  -algorithm ed25519 \
+#  -out cert/demo.key
+
+# generate an elliptic curve private key (Works in Java 21)
+# cert will have a   Signature Algorithm: ecdsa-with-SHA256
 openssl ecparam \
   -genkey \
   -name prime256v1 \
