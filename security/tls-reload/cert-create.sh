729d74236a3febb1a5f2562f4feed132fa816865
Adib Saikali <asaikali@gmail.com>
2024-07-05 02:33:55 +0200

    Polish

--- security/tls-reload/cert-create.sh
+++ security/tls-reload/cert-create.sh
@@ -6,10 +6,11 @@
 #  -out cert/demo.key
 
 # command to make an rsa based private key (Does not work in Java 21)
-# it claims there is no computable signature verification algorithm
+# jdk claims there is no computable signature verification algorithm in ssl debug log
 # Generate cert will have Signature Algorithm: ED25519
+# seem that support is not wide sparead https://security.stackexchange.com/questions/269725/what-is-the-current-april-2023-browser-support-for-ed25519-certificate-signatu
 #openssl genpkey \
-#  -algorithm ed25519 \
+#  -algorithm Ed25519 \
 #  -out cert/demo.key
 
 # generate an elliptic curve private key (Works in Java 21)
