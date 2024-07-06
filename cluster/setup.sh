b98541a3cf71827877c573f1e51c3c8679fa1b00
Adib Saikali <asaikali@gmail.com>
2024-07-06 21:53:13 +0200

    Polish

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,2 +1,7 @@
-kapp deploy -a cert-manager -f static/cert-manager/cert-manager.yaml -y
+#kapp deploy -a cert-manager -f static/cert-manager/cert-manager.yaml -y
+#kapp deploy -a contour -f static/contour -y
+
 # kapp deploy -a envoy-gateway -f https://github.com/envoyproxy/gateway/releases/download/v1.0.2/install.yaml -y
+helm template contour ./charts/network/contour --values ./values/contour.yaml  > templates/contour.yaml
+
+helm upgrade contour ./charts/network/contour --values ./values/contour.yaml --install
