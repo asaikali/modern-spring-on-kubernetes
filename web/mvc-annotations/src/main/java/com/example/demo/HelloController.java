4876b8e1be855dfd23f5976617c591fb29d604dc
Adib Saikali <asaikali@gmail.com>
2024-07-10 13:18:02 +0200

    Polish

--- web/mvc-annotations/src/main/java/com/example/demo/HelloController.java
+++ web/mvc-annotations/src/main/java/com/example/demo/HelloController.java
@@ -1,2 +1,24 @@
-package com.example.demo;public class HelloController {
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${services.hello}/time")
+public class HelloController {
+
+  // maps to /hello/time and /hello/time/ b
+  @GetMapping({ "", "/" })
+  String get() {
+    return "Time is " + LocalDateTime.now();
+  }
+
+  // maps to /hello/time/date
+  @GetMapping("/date")
+  String date() {
+    return "date is " + LocalDate.now();
+  }
 }
--- web/mvc-annotations/src/main/java/com/example/demo/MvcAnnotationsApplication.java
+++ web/mvc-annotations/src/main/java/com/example/demo/MvcAnnotationsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MvcAnnotationsApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(MvcAnnotationsApplication.class, args);
   }
 }
