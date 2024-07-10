e2613b83251a01efaf04ae646c79f8f0532152f4
Adib Saikali <asaikali@gmail.com>
2024-07-10 13:47:00 +0200

    Polish

---
+++ cluster/readme.md
@@ -0,0 +1,10 @@
+# Configuring a Kuberetes to host Spring Applications
+
+What is the optimal way to configure a Kubernetes cluster to host Spring 
+applications both for local development on a laptop and for production 
+usage. This area of the repo contains instructions for how to configure a
+Kubernetes cluster  a laptop and on cloud to host spring applications. 
+
+You are going to need a decent understanding of Kubernetes to fully understand
+what is in here. However, if you are new to Kubernetes, you should be able to 
+run the scripts.
--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,7 +1,57 @@
-#kapp deploy -a cert-manager -f static/cert-manager/cert-manager.yaml -y
-#kapp deploy -a contour -f static/contour -y
+#!/bin/bash
 
-# kapp deploy -a envoy-gateway -f https://github.com/envoyproxy/gateway/releases/download/v1.0.2/install.yaml -y
-helm template contour ./charts/network/contour --values ./values/contour.yaml  > templates/contour.yaml
+# Define the bold text format
+bold=$(tput bold)
+normal=$(tput sgr0)
 
-helm upgrade contour ./charts/network/contour --values ./values/contour.yaml --install
+# Install or upgrade CertManager
+echo "${bold}Installing or upgrading CertManager...${normal}"
+helm upgrade --install cert-manager cert-manager \
+  --repo https://charts.jetstack.io \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.15.1 \
+  --set crds.enabled=true \
+  --wait
+
+# Check if CertManager was successfully deployed
+if [ $? -eq 0 ]; then
+  echo "${bold}CertManager installed successfully.${normal}"
+else
+  echo "${bold}Failed to install CertManager.${normal}"
+  exit 1
+fi
+
+# Install or upgrade Contour
+echo "${bold}Installing or upgrading Contour...${normal}"
+helm upgrade --install contour contour \
+  --repo https://charts.bitnami.com/bitnami \
+  --namespace contour \
+  --create-namespace \
+  --version 18.2.9 \
+  --set envoy.service.type=NodePort \
+  --set envoy.service.nodePorts.http=30080 \
+  --set envoy.service.nodePorts.https=30443 \
+  --set contour.resources.requests.cpu=100m \
+  --set contour.resources.requests.memory=100Mi \
+  --set contour.resources.limits.cpu=500m \
+  --set contour.resources.limits.memory=500Mi \
+  --set envoy.resources.requests.cpu=100m \
+  --set envoy.resources.requests.memory=128Mi \
+  --set envoy.resources.limits.cpu=1000m \
+  --set envoy.resources.limits.memory=1024Mi \
+  --set envoy.shutdownManager.resources.requests.cpu=50m \
+  --set envoy.shutdownManager.resources.requests.memory=50Mi \
+  --set envoy.shutdownManager.resources.limits.cpu=250m \
+  --set envoy.shutdownManager.resources.limits.memory=256Mi \
+  --wait
+
+# Check if Contour was successfully deployed
+if [ $? -eq 0 ]; then
+  echo "${bold}Contour installed successfully.${normal}"
+else
+  echo "${bold}Failed to install Contour.${normal}"
+  exit 1
+fi
+
+echo "${bold}Installation of CertManager and Contour completed.${normal}"
