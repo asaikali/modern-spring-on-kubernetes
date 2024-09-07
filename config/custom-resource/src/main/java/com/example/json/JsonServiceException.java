cad8ca86e877f92e377e31e97ff68bde049d6ee0
Adib Saikali <asaikali@gmail.com>
2024-09-07 03:49:22 -0400

    Polish

--- config/custom-resource/src/main/java/com/example/json/JsonServiceException.java
+++ config/custom-resource/src/main/java/com/example/json/JsonServiceException.java
@@ -1,11 +1,3 @@
-/*
- * Copyright 2023 Programming Mastery Inc.
- *
- * All Rights Reserved Unauthorized copying of this file, via any medium is strictly prohibited.
- *
- * Proprietary and confidential
- */
-
 package com.example.json;
 
 /**
