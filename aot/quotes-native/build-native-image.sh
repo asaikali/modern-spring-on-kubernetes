148de852c4fbefe2aeabebae3bf2ccb0648c8a6a
Adib Saikali <asaikali@gmail.com>
2025-05-22 14:46:44 +0200

    polish

--- aot/quotes-native/build-native-image.sh
+++ aot/quotes-native/build-native-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
- ./gradlew bootBuildImage -Pnative --imageName quotes-native:aot
-# ./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=quotes-native:1
+# ./gradlew bootBuildImage -Pnative --imageName quotes-native:aot
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=quotes-native:1
 docker images boot-*
