42269ce527ffa281b162fcd0aff3dbc0bb57afbc
Adib Saikali <asaikali@gmail.com>
2025-06-12 01:45:25 -0400

    add sse example

---
+++ web/mvc-sse/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEventBuilder;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  SseEmitter sse() {
+    SseEmitter emitter = new SseEmitter(60_000l);
+    AtomicInteger count = new AtomicInteger();
+
+    Executors.newSingleThreadScheduledExecutor()
+        .scheduleAtFixedRate(
+            () -> {
+              try {
+                SseEventBuilder builder =
+                    SseEmitter.event()
+                        .data(LocalDateTime.now())
+                        .id(String.valueOf(count.getAndIncrement()))
+                        .comment("example comment")
+                        .name("time");
+
+                emitter.send(builder);
+              } catch (Exception e) {
+                emitter.completeWithError(e);
+              }
+            },
+            0,
+            5,
+            TimeUnit.SECONDS);
+
+    emitter.onCompletion(() -> System.err.println("SSE stream closed"));
+    emitter.onCompletion(() -> System.err.println("SSE emitter timed out"));
+    return emitter;
+  }
+}
---
+++ web/mvc-sse/src/main/java/com/example/demo/MvcSseApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MvcSseApplication.class, args);
+  }
+}
