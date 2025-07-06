027370652c626e65ca0aee83686596f88c98fe19
Adib Saikali <asaikali@gmail.com>
2025-07-06 14:57:07 -0400

    Polish

--- web/webflux/src/main/java/com/example/demo/WebFluxApplication.java
+++ web/webflux/src/main/java/com/example/demo/WebFluxApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactiveWebApplication {
+public class WebFluxApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ReactiveWebApplication.class, args);
+    SpringApplication.run(WebFluxApplication.class, args);
   }
 }
