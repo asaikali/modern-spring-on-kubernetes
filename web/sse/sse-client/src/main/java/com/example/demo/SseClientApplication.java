773f38f2553f4994d01e7b63a595919fab245109
Adib Saikali <asaikali@gmail.com>
2025-07-06 14:55:48 -0400

    Polish

---
+++ web/sse/sse-client/src/main/java/com/example/demo/SseClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SseClientApplication.class, args);
+  }
+}
