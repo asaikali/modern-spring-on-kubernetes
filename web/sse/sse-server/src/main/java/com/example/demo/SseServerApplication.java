22d7da6c8a7c0019fd7e081e9dd933623b9254bc
Adib Saikali <asaikali@gmail.com>
2025-07-07 18:01:47 -0400

    Polish

--- web/sse/sse-server/src/main/java/com/example/demo/SseServerApplication.java
+++ web/sse/sse-server/src/main/java/com/example/demo/SseServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvcSseApplication {
+public class SseServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MvcSseApplication.class, args);
+    SpringApplication.run(SseServerApplication.class, args);
   }
 }
