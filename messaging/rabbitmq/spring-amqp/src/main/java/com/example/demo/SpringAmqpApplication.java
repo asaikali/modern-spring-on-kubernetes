d62d8455a4d7d25445399b2fc99982534b56a80b
Adib Saikali <asaikali@gmail.com>
2025-07-16 07:48:16 -0400

    add rabbitmq demo app

--- messaging/rabbitmq/spring-amqp/src/main/java/com/example/demo/SpringAmqpApplication.java
+++ messaging/rabbitmq/spring-amqp/src/main/java/com/example/demo/SpringAmqpApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitMqApplication {
+public class SpringAmqpApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(RabbitMqApplication.class, args);
+    SpringApplication.run(SpringAmqpApplication.class, args);
   }
 }
