716615e7496fcdae1e667c9d90e5ef5ce80705f1
Adib Saikali <asaikali@gmail.com>
2025-07-16 08:30:54 -0400

    Polish

---
+++ messaging/rabbitmq/rabbit-stream/src/main/java/com/example/demo/basic/Receive.java
@@ -0,0 +1,24 @@
+package com.example.demo.basic;
+
+import com.rabbitmq.stream.Consumer;
+import com.rabbitmq.stream.Environment;
+import com.rabbitmq.stream.OffsetSpecification;
+import java.time.Duration;
+
+public class Receive {
+
+  public static void main(String[] args) {
+    Environment environment = Environment.builder().build();
+    environment.streamCreator()
+        .stream("hello-java-stream")
+        .create();
+
+    Consumer consumer  = environment.consumerBuilder()
+        .stream("hello-java-stream")
+        .offset(OffsetSpecification.first())
+        .messageHandler( (unused, message) -> {
+          System.out.println("Received Message: " + new String(message.getBodyAsBinary()));
+        })
+        .build();
+  }
+}
---
+++ messaging/rabbitmq/rabbit-stream/src/main/java/com/example/demo/basic/Send.java
@@ -0,0 +1,17 @@
+package com.example.demo.basic;
+
+import com.rabbitmq.stream.Environment;
+import com.rabbitmq.stream.Message;
+import com.rabbitmq.stream.Producer;
+
+public class Send {
+
+  public static void main(String[] args) {
+    Environment environment = Environment.builder().build();
+    environment.streamCreator().stream("hello-java-stream").create();
+    Producer producer = environment.producerBuilder().stream("hello-java-stream").build();
+    Message message = producer.messageBuilder().addData("Hello, World!".getBytes()).build();
+    producer.send(message,null);
+    System.out.println("[x] `Hello World!` message sent");
+  }
+}
