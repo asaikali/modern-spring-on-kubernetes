4b9b399831ce94c7b1d9e8ce8cc9276d629107b8
Adib Saikali <asaikali@gmail.com>
2025-07-16 08:03:40 -0400

    add rabbitmq demo app

---
+++ messaging/rabbitmq/rabbit-stream/src/main/java/com/example/demo/RabbitStreamApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitStreamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RabbitStreamApplication.class, args);
+  }
+}
