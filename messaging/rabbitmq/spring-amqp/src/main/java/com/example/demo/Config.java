fa907d0c7195a544c319975e03ea856fff6bfc57
Adib Saikali <asaikali@gmail.com>
2025-07-16 17:20:58 -0400

    Polish

--- messaging/rabbitmq/spring-amqp/src/main/java/com/example/demo/Config.java
+++ messaging/rabbitmq/spring-amqp/src/main/java/com/example/demo/Config.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import org.springframework.amqp.core.AnonymousQueue;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +15,20 @@ public class Config {
   @Bean
   Queue hello() {
     return new Queue("hello");
+  }
+
+  @Bean
+  public Queue anon() {
+    return new AnonymousQueue();
+  }
+
+  @Bean
+  FanoutExchange fanoutExchange() {
+    return new FanoutExchange("tut.fanout");
+  }
+
+  @Bean
+  Binding helloBinding(FanoutExchange exchange, @Qualifier("anon") Queue queue) {
+    return BindingBuilder.bind(queue).to(exchange);
   }
 }
--- messaging/rabbitmq/spring-amqp/src/main/java/com/example/demo/HelloController.java
+++ messaging/rabbitmq/spring-amqp/src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +12,11 @@ public class HelloController {
 
   private final RabbitTemplate rabbitTemplate;
   private final Queue helloQueue;
+  private final FanoutExchange fanoutExchange;
 
-  public HelloController(RabbitTemplate rabbitTemplate, Queue hello) {
+  public HelloController(RabbitTemplate rabbitTemplate, @Qualifier("hello") Queue hello, FanoutExchange fanoutExchange) {
     this.rabbitTemplate = rabbitTemplate;
+    this.fanoutExchange = fanoutExchange;
     this.helloQueue = hello;
   }
 
@@ -21,4 +25,11 @@ public class HelloController {
     this.rabbitTemplate.convertAndSend(helloQueue.getName(), "hello");
     return "Sent hello World";
   }
+
+  @GetMapping("/fanout")
+  public String fanout() {
+    this.rabbitTemplate.convertAndSend(fanoutExchange.getName(), "hello");
+    return "sent message on a fanout exchange";
+  }
+
 }
