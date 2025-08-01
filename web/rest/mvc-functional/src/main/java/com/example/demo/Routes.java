15d88a4cbfcf1efc0b8d8d35e4520f5e58625244
Adib Saikali <asaikali@gmail.com>
2025-08-01 19:07:10 -0400

    Polish

---
+++ web/rest/mvc-functional/src/main/java/com/example/demo/Routes.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    RouterFunction<ServerResponse> helloRoute() {
+        return RouterFunctions.route()
+            .GET("/hello", req -> ServerResponse.ok().body("Hello World"))
+            .build();
+    }
+}
