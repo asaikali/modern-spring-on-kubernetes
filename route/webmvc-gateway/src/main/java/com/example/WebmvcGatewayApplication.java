7f2a798ef06cd6972bee59b9af264b454f8f590b
Adib Saikali <asaikali@gmail.com>
2025-08-02 01:21:11 -0400

    Polish

---
+++ route/webmvc-gateway/src/main/java/com/example/WebmvcGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebmvcGatewayApplication.class, args);
+  }
+}
