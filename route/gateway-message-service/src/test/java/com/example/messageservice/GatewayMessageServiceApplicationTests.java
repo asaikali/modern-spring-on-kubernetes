5478b05d48a7ff28e1aba43cb6a3957f1929ba48
Adib Saikali <asaikali@gmail.com>
2025-08-01 23:53:59 -0400

    Polish

---
+++ route/gateway-message-service/src/test/java/com/example/messageservice/GatewayMessageServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.messageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GatewayMessageServiceApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
---
+++ route/webflux-gateway/src/test/java/com/example/WebfluxGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebfluxGatewayApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
