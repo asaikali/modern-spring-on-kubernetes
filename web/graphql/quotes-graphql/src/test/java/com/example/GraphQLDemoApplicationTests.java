f61e23b60047097e4247ed5762d3d15a487d78af
Adib Saikali <asaikali@gmail.com>
2026-07-12 21:56:29 -0400

    Fix test compilation for Boot 4 test-slice modularization
    - @WebMvcTest now lives in spring-boot-webmvc-test; declared as a test
      dependency in build-test-aot-native
    - @LocalServerPort did not move in Boot 4 (still spring-boot-test);
      revert an over-eager import change in quotes-graphql
    - drop the last Testcontainers 1.x version pin in rabbit-stream

--- web/graphql/quotes-graphql/src/test/java/com/example/GraphQLDemoApplicationTests.java
+++ web/graphql/quotes-graphql/src/test/java/com/example/GraphQLDemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.test.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContext;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
